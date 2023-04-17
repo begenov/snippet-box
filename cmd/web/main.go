dc5049e820526c175b0ecdd2ea760e644aacbdfa
Your Name <you@example.com>
2023-04-17 13:43:41 +0600

    Leveled Logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
-
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+	infoLog.Printf("Starting server on %s", *addr)
+	// Call the ListenAndServe() method on our new http.Server struct.
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
